87a86a0cbbc958a8172e9cb57228e8724e835fe3
Mikael-Dahlin <71758203+Mikael-Dahlin@users.noreply.github.com>
2020-10-13 11:16:32 +0200

    update
    changed css and js code

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 let targetButton = document.querySelector(".targetButton");
+let squareSize = 80;
+let side = `${squareSize}px`
+
+targetButton.style.height = side;
+targetButton.style.width = side;
+
+targetButton.addEventListener('click', () => {
+    // targetButton.style.display = 'none';
+    buttonPlacement();
+});
+
 const container = document.querySelector(".container");
 
 console.log(container.clientHeight);
@@ -6,9 +17,9 @@ console.log(container.clientWidth);
 console.log(targetButton.clientHeight);
 console.log(targetButton.clientWidth);
 
-const randomizer = (element) => {
-    const maxHeight = container.clientHeight - element.clientHeight;
-    const maxWidth = container.clientWidth - element.clientWidth;
+const randomizer = (element, size) => {
+    const maxHeight = container.clientHeight - size;
+    const maxWidth = container.clientWidth - size;
     const randomHeight = Math.floor(Math.random()*maxHeight);
     const randomWidth = Math.floor(Math.random()*maxWidth);
     return [randomHeight,randomWidth];
@@ -16,7 +27,7 @@ const randomizer = (element) => {
 
 const buttonPlacement = () => {    
     targetButton.style.display = 'none';
-    let randomNumbers = randomizer(targetButton)
+    let randomNumbers = randomizer(targetButton, squareSize);
     targetButton.style.top = `${randomNumbers[0]}px`;
     targetButton.style.left = `${randomNumbers[1]}px`;
     targetButton.style.display = 'block';
